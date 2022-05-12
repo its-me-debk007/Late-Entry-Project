c1dcb325a38c363bc20c9885d19d1aae1cd76485
Debashish Kundu <debashish.joy@gmail.com>
2022-05-12 22:47:05 +0530

    bottom sheet dialog fragment in bar code scanner

--- app/src/main/java/in/silive/lateentryproject/MainActivity.kt
+++ app/src/main/java/in/silive/lateentryproject/MainActivity.kt
@@ -1,11 +1,24 @@
 package `in`.silive.lateentryproject
 
+import `in`.silive.lateentryproject.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+	private lateinit var binding: ActivityMainBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+		val modalBottomSheet = BottomSheetFragment()
+		modalBottomSheet.show(supportFragmentManager, "BottomSheet")
+
+//			supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, ())
+//			.commit()
 	}
 }
