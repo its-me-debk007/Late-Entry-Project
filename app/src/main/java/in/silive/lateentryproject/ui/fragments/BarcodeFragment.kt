83e29b703be14b44b8860643361b462020f3a20c
Debashish Kundu <debashish.joy@gmail.com>
2022-05-15 19:48:16 +0530

    handled long student no.

--- app/src/main/java/in/silive/lateentryproject/ui/fragments/BarcodeFragment.kt
+++ app/src/main/java/in/silive/lateentryproject/ui/fragments/BarcodeFragment.kt
@@ -8,7 +8,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -55,7 +54,6 @@ class BarcodeFragment : Fragment(), ZBarScannerView.ResultHandler {
 
 			binding.enterStudentNoBtn.isEnabled = false
 			binding.enterStudentNoBtn.isClickable = false
-
 		}
 
 		return view
